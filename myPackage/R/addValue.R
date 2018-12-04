6a7e3f36e26af60dc0d107958a7d29fa88b5ec8b
Patrick Sogno <44399629+PatrickSog@users.noreply.github.com>
2018-12-04 12:35:52 +0100

    Addition function
    It adds numeric values.

---
+++ myPackage/R/addValue.R
@@ -0,0 +1,18 @@
+#'@title Addition
+#'
+#'@description A function that adds values.
+#'
+#'@param x One value
+#'@param y Another value
+#'
+#'@details there are no details to this
+#'
+#'@examples val <- addValue (1, 2)
+#'
+#'@return numeric value
+#'
+#'@export
+
+addValue <- function(x, y) {
+  return(x+y)
+}
